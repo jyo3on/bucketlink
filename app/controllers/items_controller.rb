9ad71a310f4dbb6d85858925a6e495d0e3e783a6
seunghwan <rr5ys5s@gmail.com>
2017-05-20 20:11:18 +0900

    new page 및 create 구현 중

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,27 @@
 class ItemsController < ApplicationController
+  def new
+     @item = Item.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render xml: @item }
+    end
+  end
+  def create
+    @item = Item.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        flash[:notice] = 'Item was successfully created.'
+        format.html { redirect_to(@item) }
+        format.xml  { render xml: @item, status: :created, location: @item }
+      else
+        format.html { render action: 'new' }
+        format.xml  { render xml: @item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  def item_params
+    params.require(:item).permit(:title, :img, :content)
+  end
 end
--- db/migrate/20170520083457_create_items.rb
+++ db/migrate/20170520083457_create_items.rb
@@ -2,7 +2,9 @@ class CreateItems < ActiveRecord::Migration[5.0]
   def change
     create_table :items do |t|
       t.references :bucket, foreign_key: true
-
+      t.string :title
+      t.string :img
+      t.text :content
       t.timestamps
     end
   end
